fcda6dc4ec95931edc4091bd0144b706ffde7ecf
Devin <devincb93@gmail.com>
2023-07-30 00:10:49 -0700

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const dataStuff = {
+      name: userName,
+      email: userEmail
+    };
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(dataStuff)
+    })
+      .then(response => response.json())
+      .then(data => {
+        
+        const newId = data.id;
+  
+        const idElement = document.createElement('p');
+        idElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(idElement);
+      })
+      .catch(error =>{
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+      })
+  }
